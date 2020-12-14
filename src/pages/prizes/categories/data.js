ea99b20bca52ffa7157667e140a9e6ff21e295cc
Mitchell Slavik <Mitchell.A.Slavik@gmail.com>
2020-12-14 10:39:30 -0600

    New categories for 2020-21

--- src/pages/prizes/categories/data.js
+++ src/pages/prizes/categories/data.js
@@ -1,32 +1,38 @@
 export default {
-  'home': {
-    title: 'Home',
-    image: 'https://images.unsplash.com/photo-1522444195799-478538b28823?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=30',
-    gif: 'https://media.giphy.com/media/d2ZjBlsQa5dWO45a/giphy.gif',
+  home: {
+    title: "Home",
+    image:
+      "https://images.unsplash.com/photo-1522444195799-478538b28823?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=30",
+    gif: "https://media.giphy.com/media/d2ZjBlsQa5dWO45a/giphy.gif",
   },
-  'electronics': {
-    title: 'Electronics',
-    image: 'https://images.unsplash.com/photo-1486611367184-17759508999c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=30',
-    gif: 'https://media.giphy.com/media/UvOaWabxo1enu/giphy.gif',
+  electronics: {
+    title: "Electronics",
+    image:
+      "https://images.unsplash.com/photo-1486611367184-17759508999c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=30",
+    gif: "https://media.giphy.com/media/UvOaWabxo1enu/giphy.gif",
   },
-  'sports': {
-    title: 'Sports',
-    image: 'https://images.unsplash.com/photo-1535132011086-b8818f016104?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=30',
-    gif: 'https://media.giphy.com/media/3oKIPyqTWsP720AOXe/giphy.gif',
+  kitchen: {
+    title: "Kitchen",
+    image:
+      "https://images.unsplash.com/photo-1556912173-3bb406ef7e77?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=30",
+    gif: "https://media.giphy.com/media/3oKIPyqTWsP720AOXe/giphy.gif",
   },
-  'night-out': {
-    title: 'Night Out',
-    image: 'https://images.unsplash.com/photo-1517957096399-3a4e3d34d95e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=30',
-    gif: 'https://media.giphy.com/media/7rj2ZgttvgomY/giphy.gif',
+  misc: {
+    title: "Misc",
+    image:
+      "https://images.unsplash.com/photo-1536604673810-81370412626d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=30",
+    gif: "https://media.giphy.com/media/7rj2ZgttvgomY/giphy.gif",
   },
-  'auto': {
-    title: 'Auto',
-    image: 'https://images.unsplash.com/photo-1504215680853-026ed2a45def?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=30',
-    gif: 'https://media.giphy.com/media/is1phHwLNTFXq/giphy.gif',
+  garage: {
+    title: "Garage",
+    image:
+      "https://images.unsplash.com/photo-1594818020972-e96e722493f7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=30",
+    gif: "https://media.giphy.com/media/is1phHwLNTFXq/giphy.gif",
   },
-  'dmsi': {
-    title: 'DMSi Favorites',
-    image: 'https://images.unsplash.com/photo-1519701106023-2c9dcad3d362?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=30',
-    gif: 'https://media.giphy.com/media/VxTdsT40Cd9yE/giphy.gif',
+  personal: {
+    title: "Personal",
+    image:
+      "https://images.unsplash.com/photo-1588720697792-dbc240fad57e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=30",
+    gif: "https://media.giphy.com/media/VxTdsT40Cd9yE/giphy.gif",
   },
-}
+};
