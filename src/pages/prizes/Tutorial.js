344bbbf28b24bafa5bbc7dbbcbf986bfd90baec8
Matt McElwee <mmcelwee@dmsi.com>
2020-12-21 15:24:20 -0500

    Removed unused imports'

--- src/pages/prizes/Tutorial.js
+++ src/pages/prizes/Tutorial.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import styled, { createGlobalStyle } from 'styled-components';
+import styled from 'styled-components';
 
 import primitives from '@wedgekit/primitives';
 
